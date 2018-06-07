09893c24fd47376a35537daa8e2cc864d9eb9115
Marwan <Marwan@apples-MacBook-Pro.local>
2018-06-08 00:40:12 +0400

    Add MACircularReveal class

---
+++ MACircularReveal/MACirculerReveal.swift
@@ -0,0 +1,13 @@
+//
+//  MACirculerReveal.swift
+//  MACircularReveal
+//
+//  Created by Marwan on 12/2/17.
+//  Copyright © 2017 Marwan. All rights reserved.
+//
+
+import UIKit
+
+class MACirculerReveal: NSObject {
+
+}
